d06670b96ed6189010a4ad4bcae396e7293609d0
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-10-03 14:39:25 +0300

    nezinau kas cia buvo

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Movie;
+use App\Models\MovieImage;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -42,11 +43,32 @@ class MovieController extends Controller
      */
     public function store(Request $request)
     {
-       Movie::create([
-        'title' => $request->title,
-        'price' => $request->price,
-        'category_id' => $request->category_id,
-       ]);
+       
+            if ($request->file('photo')) {
+                $id = $movie->id;
+                $urls = [];
+                foreach($request->file('photo') as $photo) {
+
+                
+
+
+                $ext = $photo->getClientOriginalExtension();
+                $name = pathinfo($photo->getClientOriginalName(), PATHINFO_FILENAME);
+                $file = $name. '-' . rand(100000, 999999). '.' . $ext;
+                
+                // $Image = Image::make($photo)->pixelate(12);
+                // $Image->save(public_path().'/images/'.$file);
+                $photo->move(public_path().'/images', $file);
+                }
+            }
+            $movie = Movie::create([
+                'url' => asset('/images/') . '/' . $file,
+                'movie_id' => $id
+                
+               ]);
+            MovieImage::
+
+
        return redirect()->route('m_index');
     }
 
--- resources/views/movie/create.blade.php
+++ resources/views/movie/create.blade.php
@@ -2,34 +2,34 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-5">
+        <div class="col-8">
             <div class="card">
                 <div class="card-header">
                     <h2>New Movie</h2>
                 </div>
                 <div class="card-body">
                     <form action="{{route('m_store')}}" method="post" enctype="multipart/form-data">
-                    <div class="input-group mb-3">
-                        <span class="input-group-text">Title</span>
-                        <input value="{{old('title')}}" name="title" type="text" class="form-control">
-                    </div>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text">Price</span>
-                        <input value="{{old('price')}}" name="price" type="text" class="form-control">
-                    </div>
-                    <select name="mechanic_id" class="form-select mt-3">
-                        <option value="0">Choose category</option>
-                        @foreach ($categories as $category)
-                        <option value="{{$category->id}}" @if($category->id == old('category_id')) selected @endif>{{$category->title}}</option>
-                        @endforeach
+                        <div class="input-group mb-3">
+                            <span class="input-group-text">Title</span>
+                            <input value="{{old('title')}}" name="title" type="text" class="form-control">
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text">Price</span>
+                            <input value="{{old('price')}}" name="price" type="text" class="form-control">
+                        </div>
+                        <select name="mechanic_id" class="form-select mt-3">
+                            <option value="0">Choose category</option>
+                            @foreach ($categories as $category)
+                            <option value="{{$category->id}}" @if($category->id == old('category_id')) selected @endif>{{$category->title}}</option>
+                            @endforeach
 
-                      </select>
-                      {{-- <div class="input-group mt-3">
-                        <span class="input-group-text">Upload photo</span>
-                        <input type="file" name="photo" class="form-control">
-                    </div> --}}
-                    @csrf
-                    <button type="submit" class="btn btn-secondary mt-4">Create</button>
+                        </select>
+                        <div data-clone class="input-group mt-3">
+                            <span class="input-group-text">Upload Img</span>
+                            <input type="file" name="photo[]" multiple class="form-control">
+                        </div>
+                        @csrf
+                        <button type="submit" class="btn btn-secondary mt-4">Create</button>
                     </form>
                 </div>
             </div>
